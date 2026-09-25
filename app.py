5aa76d3c8281416ad3d7720f72b3427d8cd2adce
Gayathiri R <144547569+asanm104anm10410422213011@users.noreply.github.com>
2026-09-25 21:38:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
-app = Flask(__name__) 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
-  return """ 
-  <h1>AI-Based Blood Bank Inventory System</h1> 
-  <p>Welcome to the Blood Bank Inventory Management System.</p> 
-  <p>Project Status: Under Development</p> 
-  """ 
-  if __name__ == "__main__": 
+    return render_template("index.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
