6b6698a192ca7e0aa9f97bb4934c9173e42aad7b
Gayathiri R <144547569+asanm104anm10410422213011@users.noreply.github.com>
2026-09-26 15:12:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+blood_stock = {
+    "A+": 10,
+    "A-": 5,
+    "B+": 8,
+    "B-": 4,
+    "O+": 12,
+    "O-": 3,
+    "AB+": 6,
+    "AB-": 2
+}
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        blood_group = request.form["blood_group"]
+        units = int(request.form["units"])
+        blood_stock[blood_group] += units
+        return redirect("/")
+
+    return render_template("index.html", blood_stock=blood_stock)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
